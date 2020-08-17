4dcd5b77d57530b26b432e10d7c6627c919cc515
liujinaus <841162460@qq.com>
2020-08-17 18:20:52 +0800

    frist ros commit

---
+++ catkin_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/liujin/workspace/ros_program/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/liujin/workspace/ros_program/catkin_ws/devel;/home/liujin/workspace/0817/devel;/opt/ros/melodic'.split(';') if '/home/liujin/workspace/ros_program/catkin_ws/devel;/home/liujin/workspace/0817/devel;/opt/ros/melodic' != '' else []
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export LD_LIBRARY_PATH="/home/liujin/workspace/0817/devel/lib:/opt/ros/melodic/lib:/usr/local/cuda-9.0/lib64/home/liujin/workspace/catkin_ws/devel/lib:/usr/local/cuda-9.0/lib64"
+export PKG_CONFIG_PATH="/home/liujin/workspace/0817/devel/lib/pkgconfig:/opt/ros/melodic/lib/pkgconfig"
+export PWD="/home/liujin/workspace/ros_program/catkin_ws/build"
+export ROS_PACKAGE_PATH="/home/liujin/workspace/ros_program/catkin_ws/src:/home/liujin/workspace/0817/src:/opt/ros/melodic/share"
